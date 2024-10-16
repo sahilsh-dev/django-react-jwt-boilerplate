34a943375236116d95e8607abffdb67210c9491a
Sahil Sharma <vlsharma713@gmail.com>
2024-10-16 10:23:46 +0530

    Update home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,73 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 function Home() {
+	const [notes, setNotes] = useState([]);
+	const [content, setContent] = useState("");
+	const [title, setTitle] = useState("");
+
+	useEffect(() => {
+		getNotes();
+	}, []);
+
+	const getNotes = async () => {
+		await api
+			.get("/api/notes/")
+			.then((res) => res.data)
+			.then((data) => {
+				setNotes(data);
+				console.log(data);
+			})
+			.catch((err) => console.log(err));
+	};
+
+	const deleteNote = async (id) => {
+		await api
+			.delete(`/api/notes/${id}/`)
+			.then(() => getNotes())
+			.catch((err) => console.log(err));
+	};
+
+	const addNote = async (e) => {
+		e.preventDefault();
+		await api
+			.post("/api/notes/", { title, content })
+			.then(() => getNotes())
+			.catch((err) => console.log(err));
+	};
+
 	return (
 		<div>
-			<h1>Home</h1>
+			<div>
+				<h2>Notes</h2>
+			</div>
+			<h2>Create a Note</h2>
+			<form onSubmit={addNote}>
+				<label htmlFor="title">Title</label>
+				<br />
+				<input
+					type="text"
+					id="title"
+					name="title"
+					required
+					onChange={(e) => setTitle(e.target.value)}
+					value={title}
+				/>
+				<br />
+				<label htmlFor="content">Content</label>
+				<br />
+				<textarea
+					id="content"
+					name="content"
+					required
+					value={content}
+					onChange={(e) => {
+						setContent(e.target.value);
+					}}
+				></textarea>
+				<br />
+				<input type="submit" value="Submit" />
+			</form>
 		</div>
 	);
 }
